7a1123acc074e13a100fb9d23e4e89a9ffe5d52e
Adrian Kaczmarek <adishpl@gmail.com>
2024-04-05 01:50:45 +0200

    Initialize GitHubUsersApp

---
+++ DemoApps/GitHubUsers/GitHubUsers/GitHubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersApp.swift
+//  GitHubUsers
+//
+//  Created by Adrian Kaczmarek on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
